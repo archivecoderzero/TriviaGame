ad315eda576cc02e66a4ec1fea728a077bbe4ae9
unknown <archivecoderzero@gmail.com>
2019-04-22 21:55:06 -0700

    psuedo-code on js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// PSUEDO CODE //
+
+// Create variables, equeal to the object, of which the randomizer will pick the order of the questions that is going to be displayed .
+
+// Create a timer that counts backwards.it is initalized by the "start now " button from the jumbrtron .when that time strikes zero, it will display message which tells the player that he lost a turn. Will come back to this later.
+
+// Create a question that has 4 choices , the user will pick 1 out of the 4 and it will display if the user has it right , incorporate the time on each question, when the user picks an answer , display corresponding choices, probably you need to create an object here. if you want to randomize the questions being asked , you probably need to put in an object . 
+
+// Create an event whenever the user click an answer, it verifies if the the answert clicked was the correct answer. for fun stuff , you can also put in an onclick event whenver the user clicks outside the range , like change the css backround or make the background static .
+
+// Every time the user clicks a "choice" , the function verifies if it is the right or wrong answer and tells the user by displaying a funny gif . 
+
+// after questions , show the stats of the player and then reroll the function to pick from the object at random , if you have time , you can also have the user pick the theme of questions , then change the css background based on that choice,,,IF YOU HAVE TIME ..
+
+// for fun stuff , include something like a music and a sound when the user picks something.. 
